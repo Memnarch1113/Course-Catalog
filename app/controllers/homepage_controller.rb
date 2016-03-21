c7ee1f66cc7499317b080cc45c6761dea2a919a2
Gabriel Seltzer <gabeseltzer@gmail.com>
2016-03-21 17:38:37 -0400

    fixed issue when not logged in

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,5 +1,9 @@
 class HomepageController < ApplicationController
   def index
+    if signed_in?
     @enrolements = Enrolement.where(:user_id => current_user.id)
+    else
+      @enrolements = []
+    end
   end
 end
