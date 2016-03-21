8d200f530f244faddb9da9e64b19213e6b0fd6e0
Gabriel Seltzer <gabeseltzer@gmail.com>
2016-03-21 12:32:23 -0400

    search is working

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,29 @@
 class SearchController < ApplicationController
   def show
   end
+
+
   def results
-    @query_subject = Subject.find_by_subject_id params[:subject][:subject_id]
-    @query_course
-    @courses = @query_subject.courses.find_all
+    @additional = "No Results"
+    query_subject_id = params[:subject][:subject_id]
+    query_course_name = params[:course_name]
+    courses1 = []
+    courses2 = []
+    if query_subject_id != ''
+      query_subject = Subject.find_by_subject_id query_subject_id
+      courses1 = query_subject.courses.all
+    end
+    if query_course_name != ''
+      courses2 = Course.where(:name => query_course_name)
+    end
+
+    if query_course_name != '' && query_subject_id != ''
+      @courses = courses1 & courses2
+    else
+      @courses = courses1|courses2
+    end
+    if @courses != []
+      @additional = "Search Results"
+    end
   end
 end
